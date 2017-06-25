138b910d09b0a1ef3f2e861726d28dd11aef2daf
RockyLee <liyansong@weizoom.com>
2017-06-25 23:15:30 +0800

    update use files

---
+++ app/components/ProductBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// var ProductBox = React.createClass({
+//     render: function () {
+//         return (
+//             <div className="productBox">
+//                 Hello World!!!!
+//             </div>
+//         );
+//     }
+// });
--- app/entry.js
+++ app/entry.js
@@ -1,16 +1,22 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 require('jquery/dist/jquery.js');
 require('bootstrap/dist/js/bootstrap.js');
 require('bootstrap/dist/css/bootstrap.css');
+import HeaderNavigation from './components/HeaderNavigation.jsx';
+import ProductBox  from './components/ProductBox.jsx';
 
 require("./style.css");
 // document.write(require("./content.js"));
 $("#bootstrap").append("<div>import bootstrap</div>")
 
-
-var AppComponent = require('./components/productBox.jsx');
+// ReactDOM.render(
+// 	<productBox />,
+// 	document.getElementById("content")
+// );
 ReactDOM.render(
-	<AppComponent />,
-	document.getElementById("content")
+  <div>
+    <HeaderNavigation />
+  </div>
+  , document.getElementById('app')
 );
