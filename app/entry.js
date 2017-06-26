d2be76c8f8efe95c130be16dc3ca4da8fb9a0bfe
RockyLee <liyansong@weizoom.com>
2017-06-26 23:53:41 +0800

    add use files

--- app/entry.js
+++ app/entry.js
@@ -4,19 +4,15 @@ require('jquery/dist/jquery.js');
 require('bootstrap/dist/js/bootstrap.js');
 require('bootstrap/dist/css/bootstrap.css');
 import HeaderNavigation from './components/HeaderNavigation.jsx';
-import ProductBox  from './components/ProductBox.jsx';
+import buttonsInstance  from './components/ProductBox.jsx';
 
 require("./style.css");
-// document.write(require("./content.js"));
-$("#bootstrap").append("<div>import bootstrap</div>")
 
-// ReactDOM.render(
-// 	<productBox />,
-// 	document.getElementById("content")
-// );
+ReactDOM.render(buttonsInstance, document.getElementById('header'));
+
 ReactDOM.render(
   <div>
     <HeaderNavigation />
   </div>
-  , document.getElementById('app')
+  , document.getElementById('navigation')
 );
